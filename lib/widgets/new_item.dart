2e4fccc63722dea01547a89d8d1d7f939600d0a3
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-06-17 11:26:38 +0530

    resolving get request frequent loading.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,27 +19,35 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItems() {
+  void _saveItems() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https('shopping-list-mac-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(url,
-          headers: {'Content-Type': 'application/json'},
-          body: json.encode(
-            {
-              'name': _enteredName,
-              'quantity': _enteredQuantity,
-              'category': _selectedCategory.title,
-            },
-          ));
-      Navigator.of(context).pop(
-        GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory),
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
       );
+
+      final Map<String, dynamic> resData = json.decode(response.body);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop(GroceryItem(
+          id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
+      // Navigator.of(context).pop();
     }
   }
 
